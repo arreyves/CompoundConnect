36951fe4913727899452a1902e9f8ffb56970da4
arreyves <arreyves@gmail.com>
2020-09-17 20:39:06 -0230

    updates new script

--- R-code/runtime_functions.R
+++ R-code/runtime_functions.R
@@ -8,7 +8,33 @@ library(fs)
 
 file_paths <- fs::dir_ls('../Data')
 
-file_paths
+file_paths <- file_paths[1:7]
+
+# 1.0 FOR LOOP ----
+
+file_contents <- list()
+
+for (i in seq_along(file_paths)) {
+  file_contents[[i]] <- read.table(
+    file = file_paths[[i]]
+  )
+}
+
+file_contents <- set_names(file_contents, file_paths)
+
+file_contents[1:7]
+
+file_contents %>%  
+  map_df(function(x) {
+    as.data.frame(x)
+    print(x)
+      }) -> tmp
+
+############ Create new Data frame for Testing
+
+
+new_plastic_spec <- rename(tmp, Raman_shift = 'V1', Intensity = 'V2') %>% head()
+
 
 
     
