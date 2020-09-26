baeca2bc3d8c6e06d994564d792049cb8b28710d
arreyves <arreyves@gmail.com>
2020-09-26 11:35:09 -0230

    deleted uneeded

--- R-code/Environment.R
+++
@@ -1,8 +0,0 @@
-# Tidy Machine Learning in R
-There’s a new modeling pipeline in town... Tidymodels. 
-Over the past few years, tidymodels have been gradually emerging as the tidyverse’s machine learning toolkit.
-## Packages required to build 
-library(tidymodels)
-library(tidyverse)
-library(workflows)
-library(tune)
